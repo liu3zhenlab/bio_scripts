8f80f67cfd800e57e75a939807dc23a06362514d
Sanzhen Liu <liu3zhen@ksu.edu>
2019-07-05 23:03:17 -0500

    Create FastaAlternateReferenceMaker.sh

---
+++ GATK_pipelines/FastaAlternateReferenceMaker.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# the script is to generate a new sequence with replacements the old sequence with variants in the vcf file
+
+# input
+oldseq=old.seq
+vcf=variants.vcf
+newseq=new.fasta
+log=vcf2fas.log
+
+# run
+gatk FastaAlternateReferenceMaker -R $oldseq -V $vcf -O $newseq &>$log
