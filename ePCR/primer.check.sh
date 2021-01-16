bf4fee4facf09152cd3425087ecd4895fe535cd0
liu3zhenlab <liu3zhen@ksu.edu>
2021-01-16 11:43:56 -0600

    ePCR

--- ePCR/primer.check.sh
+++ ePCR/primer.check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-pfas=primer.fas
-ref=/homes/liu3zhen/references/B73Ref4/bowtie/B73Ref4
-prefix=primer.onRef
+pfas=primers.fas
+ref=refdb
+prefix=out
 perl primer.ePCR.pl -p $pfas -r $ref 1>${prefix}.txt 2>${prefix}.log
 
